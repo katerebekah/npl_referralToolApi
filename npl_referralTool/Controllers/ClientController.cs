278bd5a0e0f9d001eabfbd5c2102e012cd4b177a
Kate <katerebekah@gmail.com>
2016-10-13 22:16:01 -0500

    added registered client repo and controller from branch

---
+++ npl_referralTool/Controllers/ClientController.cs
@@ -0,0 +1,66 @@
+﻿using npl_referralTool.DAL;
+using npl_referralTool.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace npl_referralTool.Controllers
+{
+    public class ClientController : ApiController
+    {
+        private RegisteredClientRepository _registeredClientRepo { get; set; }
+
+        public ClientController(RegisteredClientRepository repo)
+        {
+            _registeredClientRepo = repo;
+        }
+
+        [HttpGet]
+        [Route("client")]
+        public List<RegisteredClient> Get()
+        {
+            return _registeredClientRepo.GetAll();
+        }
+
+        [HttpGet]
+        [Route("client/{id}")]
+        public RegisteredClient GetByID(int id)
+        {
+            return _registeredClientRepo.GetById(id);
+        }
+
+        [HttpGet]
+        [Route("client/{email}")]
+        public RegisteredClient GetByEmail(string email)
+        {
+            return _registeredClientRepo.GetByEmail(email);
+        }
+
+        [HttpPost]
+        [Route("client/")]
+        [Authorize]
+        public void AddClient(RegisteredClient client)
+        {
+            _registeredClientRepo.AddClient(client);
+        }
+
+        [HttpPut]
+        [Route("client/")]
+        [Authorize]
+        public void UpdateClient(RegisteredClient client)
+        {
+            _registeredClientRepo.Update(client);
+        }
+
+        [HttpDelete]
+        [Route("client")]
+        [Authorize]
+        public void Delete(int clientId)
+        {
+            _registeredClientRepo.Delete(clientId);
+        }
+    }
+}
---
+++ npl_referralTool/DAL/RegisteredClientRepository.cs
@@ -0,0 +1,71 @@
+﻿using npl_referralTool.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace npl_referralTool.DAL
+{
+    public class RegisteredClientRepository
+    {
+        private ReferralContext _context = new ReferralContext();
+
+        public List<RegisteredClient> GetAll()
+        {
+            return _context.RegisteredClients.ToList();
+        }
+
+        public RegisteredClient GetByEmail(string email)
+        {
+            RegisteredClient client = (from u in _context.RegisteredClients
+                                       where u.EmailAddress == email
+                                       select u).FirstOrDefault();
+
+            return client;
+        }
+
+        public RegisteredClient GetById(int clientID)
+        {
+            RegisteredClient client = (from u in _context.RegisteredClients
+                                       where u.RegisteredClientID == clientID
+                                       select u).FirstOrDefault();
+            return client;
+        }
+
+        public void AddClient(RegisteredClient client)
+        {
+            _context.RegisteredClients.Add(client);
+            _context.SaveChanges();
+        }
+        public void Delete(int ClientID)
+        {
+            var client = (from u in _context.RegisteredClients
+                          where u.RegisteredClientID == ClientID
+                          select u).FirstOrDefault();
+            _context.RegisteredClients.Remove(client);
+            _context.SaveChanges();
+        }
+        public void Update(RegisteredClient modifyClient)
+        {
+            var client = (from u in _context.RegisteredClients
+                          where u.RegisteredClientID == modifyClient.RegisteredClientID
+                          select u).FirstOrDefault();
+            if (!client.Equals(modifyClient))
+            {
+                client.AreaCode = modifyClient.AreaCode;
+                client.AreaCode2 = modifyClient.AreaCode2;
+                client.EmailAddress = modifyClient.EmailAddress;
+                client.FirstName = modifyClient.FirstName;
+                client.HouseNumber = modifyClient.HouseNumber;
+                client.LastName = modifyClient.LastName;
+                client.PhoneNumber1 = modifyClient.PhoneNumber1;
+                client.PhoneNumber2 = modifyClient.PhoneNumber2;
+                client.Referrals = modifyClient.Referrals;
+                client.StreetAddress = modifyClient.StreetAddress;
+                client.ZipCode = modifyClient.ZipCode;
+
+                _context.SaveChanges();
+            }
+        }
+    }
+}
